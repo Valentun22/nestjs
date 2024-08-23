dee895156e4e617e509366fbf977be26b3506edc
Valentun22 <666valentun666@gmail.com>
2024-08-23 11:26:01 +0300

    ls2

---
+++ src/modules/posts/posts.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PostsListReqDto } from './dto/posts-list.req.dto';
+
+@Injectable()
+export class PostsService {
+  create(createCarDto: CreatePostDto) {
+    return 'This action adds a new car';
+  }
+
+  findAll(query: PostsListReqDto) {
+    return `This action returns all cars`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} car`;
+  }
+
+  update(id: number, updateCarDto: UpdatePostDto) {
+    return `This action updates a #${id} car`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} car`;
+  }
+}
